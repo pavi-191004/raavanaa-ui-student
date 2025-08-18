52a773d30f846c66b212e31df8053c7c0e3445a2
nesasheeba38 <nesasheebaj32@gmail.com>
2025-08-18 17:37:25 +0530

    MF plugin added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  federation({
+            name: 'ui-student',
+            filename: 'remoteEntry.js',
+            exposes: {
+                './App': './src/App.tcx',
+            },
+            shared: ['react','react-dom']
+        })
+  ],
+ build:{
+ target:'esnext'
+ }
 })
